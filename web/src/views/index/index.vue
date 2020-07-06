14e5f98e6768ea01d4d997decdc128e5e3d200ed
lijiahui <2836755307@qq.com>
2020-07-06 11:31:46 +0800

    搭建前端目录和最初化的东西

---
+++ web/src/views/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+/**
+@description: 首页
+@createTime:2020-07-06
+@create:lijiahui
+*/
+export default {
+  name: 'index',
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  methods: {}
+};
+</script>
+<style scoped></style>
